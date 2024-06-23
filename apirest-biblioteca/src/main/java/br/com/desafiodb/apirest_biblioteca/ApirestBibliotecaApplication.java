c889a3dbc02b91a3f78e7396d3b5a60ea25c403d
Ana Caroline Cagliari Cappellari <carolinecagliari@gmail.com>
2024-06-23 09:34:38 -0300

    Criando projeto Spring

---
+++ apirest-biblioteca/src/main/java/br/com/desafiodb/apirest_biblioteca/ApirestBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafiodb.apirest_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestBibliotecaApplication.class, args);
+	}
+
+}
---
+++ apirest-biblioteca/src/test/java/br/com/desafiodb/apirest_biblioteca/ApirestBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafiodb.apirest_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
